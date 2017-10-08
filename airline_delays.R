04f6788d1804c84c100803b31858b445d83d0198
Fungie <fergusa@tcd.ie>
2017-10-08 21:57:35 +0100

    Finished basic GLM

--- airline_delays.R
+++ airline_delays.R
@@ -7,6 +7,8 @@ library(testthat)
 library(stringr)
 library(lubridate)
 library(lazyeval)
+library(FactoMineR)
+library(caret)
 
 # Reading in data
 setwd("/Users/aurenferguson/Documents/Predicting_airline_delays/")
@@ -31,13 +33,14 @@ data <- data %>% filter(!is.na(ArrDelay))
 # Correlation between arrival and departure delay times
 cor(data$ArrDelay, data$DepDelay)
 
-# plotting departure delay time vs arrival delay time
-ggplot(data, aes(x = DepDelay, y = ArrDelay)) +
-  geom_point()
+# # plotting departure delay time vs arrival delay time
+# ggplot(data, aes(x = DepDelay, y = ArrDelay)) +
+#   geom_point()
 
 
 # quantiles of ArrDelay
 quantile(data$ArrDelay, probs = seq(0,1,0.05))
+
 
 # capping ArrDelay
 data <- data %>% mutate(ArrDelay = ifelse(ArrDelay < -20, -20,
@@ -113,6 +116,130 @@ ggplot(airports, aes(x = reorder(Origin, mean_diff), y = mean_diff, fill = Origi
 # what are biggest factors that contribute to a delay
 # to do
 
+# Preparing data for clustering --------------------------------------------
+# finding columns with only one unique value
+unique_cols <- which(sapply(data, function(x){length(unique(x))}) == 1)
+
+# removing them
+data <- data %>% select(-one_of(names(unique_cols)))
+
+# removing other columns
+data <- data %>% select(-V1, -DayofMonth, -DepTime, -FlightNum, -ActualElapsedTime, -DepDelay, -(TaxiIn:LateAircraftDelay), -ArrDelay, -AirTime, -ArrTime, - TailNum)
+
+# feature engineering
+# Time of departure would be an obvious reason why flights are delayed, i.e later flights are more likely to be delayed due to knock on from morning
+# The exact departure time is perhaps too granular. Instread, we will take the departure time
+
+# taking only the hour 
+data <- data %>% mutate( CRSDepTime = trunc(CRSDepTime / 100),
+                           CRSArrTime = trunc(CRSArrTime / 100))
+
+# Clustering --------------------------------------------------------------
+# #### K-means ------------------------------------------------------------
+
+# taking continous columns
+data_contin <- data %>% select(CRSDepTime, CRSArrTime, CRSElapsedTime, Distance)
+
+# scaling the data
+data_contin <- scale(data_contin)
+
+# Initial Kmeans
+# It would be nice if we could separate the data into 2 clusters, delayed and not delayed
+set.seed(876)
+k_means_2 <- kmeans(x =  data_contin, centers = 2, nstart = 20)
+
+# comparing cluster value to delayed marker
+prop.table(table(data$delay_marker, k_means_2$cluster), margin = 2) # two clusters dont predict if flight is delayed v well
+
+# Checking other values of k
+# Initialize total within sum of squares error
+wss <- 0
+
+# For 1 to 5 cluster centers
+for (i in 1:5) {
+  k_means <- kmeans(data_contin, centers = i, nstart = 20)
+
+  wss[i] <- k_means$tot.withinss
+}
+
+# Plot total within sum of squares vs. number of clusters
+plot(1:5, wss, type = "b",
+     xlab = "Number of Clusters",
+     ylab = "Within groups sum of squares")
+
+# An elbow appears at 3 clusters which is an interesting result
+set.seed(6364)
+k_means_3 <- kmeans(data_contin, centers = 3 , nstart = 20)
+table(data$delay_marker, k_means_3$cluster)
+prop.table(table(data$delay_marker, k_means_3$cluster), margin = 2) # cluster 3 (in this case) has a higher proportion of delayed, this could be useful for our logistic regression model
+#
+# visualisting results
+# since more than 2 variables will use PCA
+# Perform scaled PCA: pr.out
+pca_data_contin <- prcomp(data_contin, scale = F)
+
+# Inspect model output
+summary(pca_data_contin)
+
+# visualising the pca results
+# Variability of each principal component
+pr_var <- pca_data_contin$sdev ^ 2
+
+# Variance explained by each principal component: pve
+pve <- pr_var / sum(pr_var)
+
+# Plot variance explained for each principal component
+plot(pve, xlab = "Principal Component",
+     ylab = "Proportion of Variance Explained",
+     ylim = c(0, 1), type = "b")
+
+# Plot cumulative proportion of variance explained
+plot(cumsum(pve), xlab = "Plrincipal Component",
+     ylab = "Cumulative Proportion of Variance Explained",
+     ylim = c(0, 1), type = "b")
+
+pred <- as.data.frame(predict(pca_data_contin, newdata=data_contin))
+
+component_df <- data.frame(pca1 = pred$PC1,
+                           pca2 = pred$PC2,
+                           cluster = k_means_3$cluster)
+
+# taking sample for scatterplot
+# creating index
+component_df$index <- 1:nrow(component_df)
+
+# setting seed and taking sample from index for train
+set.seed(97843)
+index_component <- sample(component_df$index, 1000)
+
+component_df_slim <- component_df %>% filter(index %in% index_component)
+
+ggplot(component_df_slim,aes(x=pca1,y=pca2, color = as.factor(cluster))) + 
+  geom_point()
+
+######################
+# try pca then clustering
+k_means_pca <- kmeans(component_df %>% select(pca1,pca2), centers = 2, nstart = 20)
+prop.table(table(data$delay_marker, k_means_pca$cluster), margin = 2) # achieved same results with two clusters than above
+
+# appending pca clustering to component
+component_df$cluster_pca <- k_means_pca$cluster
+component_df$delay_marker <- data$delay_marker
+
+set.seed(97843)
+index_component <- sample(component_df$index, 1000)
+
+component_df_slim <- component_df %>% filter(index %in% index_component)
+
+ggplot(component_df_slim,aes(x=pca1,y=pca2, color = as.factor(cluster_pca), shape = as.factor(delay_marker))) + 
+  geom_point()
+
+# slimming down component_df for only columns we want for model
+component_df <- component_df %>% select(pca1, pca2, cluster_pca)
+
+# append cols to data
+data <- bind_cols(data, component_df)
+
 # Splitting the data into train and test sets -----------------------------
 # creating index
 data$index <- 1:nrow(data)
@@ -132,139 +259,93 @@ testthat::test_that("Length of train and test sets equal the base dataset",{
 })
 
 
-# Preparing data for modelling --------------------------------------------
-# finding columns with only one unique value
-unique_cols <- which(sapply(train, function(x){length(unique(x))}) == 1)
+# Logistic Regression model -----------------------------------------------
 
-# removing them
-train <- train %>% select(-one_of(names(unique_cols)))
-
-# removing other columns
-train <- train %>% select(-V1, -DayofMonth, -DepTime, -FlightNum, -ActualElapsedTime, -DepDelay, -(TaxiIn:LateAircraftDelay), -ArrDelay, -ArrTime)
-
-# feature engineering
-# Time of departure would be an obvious reason why flights are delayed, i.e later flights are more likely to be delayed due to knock on from morning
-# The exact departure time is perhaps too granular. Instread, we will take the departure time
-
-# dropping rows with bad departure date column
-train <- train %>% filter(nchar(CRSDepTime) >= 3 & nchar(CRSDepTime) <= 4)
-
-# taking only the hour 
-train <- train %>% mutate( CRSDepTime = str_sub(CRSDepTime, 1, nchar(CRSDepTime) - 2),
-                           CRSDepTime = as.integer((CRSDepTime)))
-
-train <- train %>% filter(nchar(CRSArrTime) >= 3 & nchar(CRSArrTime) <= 4)
-train <- train %>% mutate( CRSArrTime = str_sub(CRSArrTime, 1, nchar(CRSArrTime) - 2),
-                           CRSArrTime = as.integer((CRSArrTime)))
-
-# creating scores for factor variables in order for them to be allowed 
-# Month
-month_change <- train %>% group_by(Month) %>%
-  summarise(n_flights = n(),
+# reducing number of level in Origin and Destination variables
+# origin
+airports_origin_model <- train %>%
+  group_by(Origin) %>%
+  summarise(
+            n_airports = n(),
             no_delayed = sum(delay_marker),
-            amt_delay = no_delayed / n_flights)
+            amt_delay = no_delayed / n_airports) %>%
+  
+  arrange(amt_delay) %>% 
+  
+  mutate(
+          Origin_alter =  ifelse(amt_delay <= 0.28, 1,
+                          ifelse(amt_delay > 0.28 & amt_delay <=  0.35, 2,
+                          ifelse(amt_delay > 0.35 & amt_delay <= 0.4, 3, 
+                          ifelse(amt_delay > 0.4 & amt_delay <= 0.45, 4, 5))))) %>%
+  
+  select(Origin, Origin_alter)
 
-# mean amount delayed
-mean_month <- mean(month_change$amt_delay)
+# joining back
+train <- inner_join(train, airports_origin_model, by = 'Origin')
 
-# calculating score
-month_change <- month_change %>% mutate(Month_score = round(amt_delay - mean_delay,4)) %>%
-  select(Month, Month_score)
+# Dest
+airports_dest_model <- train %>%
+  group_by(Dest) %>%
+  summarise(
+    n_airports = n(),
+    no_delayed = sum(delay_marker),
+    amt_delay = no_delayed / n_airports) %>%
+  
+  arrange(amt_delay) %>% 
+  
+  mutate(
+    Dest_alter = ifelse(amt_delay <= 0.28, 1,
+                 ifelse(amt_delay > 0.28 & amt_delay <=  0.32, 2,
+                 ifelse(amt_delay > 0.32 & amt_delay <= 0.36, 3, 
+                 ifelse(amt_delay > 0.36 & amt_delay <= 0.40, 4, 5))))) %>%
+  
+  select(Dest, Dest_alter)
 
-# joining back to train
-train <- inner_join(train, month_change, on = 'Month')
+# joining back
+train <- inner_join(train, airports_dest_model, by = 'Dest')
 
-##########################
-# Day of week
-day_change <- train %>% group_by(DayOfWeek) %>%
-  summarise(n_flights = n(),
-            no_delayed = sum(delay_marker),
-            amt_delay = no_delayed / n_flights)
+# Airline
+airline_model <- train %>%
+  group_by(UniqueCarrier) %>%
+  summarise(
+    n_airports = n(),
+    no_delayed = sum(delay_marker),
+    amt_delay = no_delayed / n_airports) %>%
+  
+  arrange(amt_delay) %>% 
+  
+  mutate(
+    Carrier_alter = ifelse(amt_delay <= 0.3, 1,
+                    ifelse(amt_delay > 0.3 & amt_delay <=  0.35, 2,
+                    ifelse(amt_delay > 0.35 & amt_delay <= 0.395, 3, 4))))  %>%
+  
+  select(UniqueCarrier, Carrier_alter)
 
-# mean amount delayed
-mean_day <- mean(day_change$amt_delay)
+# joining back
+train <- inner_join(train, airline_model, by = 'UniqueCarrier')
 
-# calculating score
-day_change <- day_change %>% mutate(Day_score = round(amt_delay - mean_delay,4)) %>%
-  select(DayOfWeek, Day_score)
+# Altering Day and Month variables
+train <- train %>% mutate(Month_alter = ifelse(Month == 10 | Month == 9 | Month == 5, 1,
+                                        ifelse(Month == 4 | Month == 8 | Month == 11, 2,
+                                        ifelse(Month == 3 | Month == 1 | Month == 7, 3, 4))),
+                          
+                          Day_alter = ifelse(DayOfWeek == 3 | DayOfWeek == 6, 1,
+                                      ifelse(DayOfWeek == 4 | DayOfWeek == 1, 2, 3)),
+                          
+                          cluster_pca = cluster_pca - 1
+  
+)
 
-# joining back to train
-train <- inner_join(train, day_change, on = 'DayOfWeek')
+# Model formula
+logistic_form <- as.formula(delay_marker ~ Month_alter +
+                                           Day_alter +
+                                           pca1 +
+                                           pca2 +
+                                           Carrier_alter + 
+                                           cluster_pca +
+                                           Origin_alter +
+                                           Dest_alter)
 
-######################
-# carrier
-carrier_change <- train %>% group_by(UniqueCarrier) %>%
-  summarise(n_flights = n(),
-            no_delayed = sum(delay_marker),
-            amt_delay = no_delayed / n_flights)
-
-# mean amount delayed
-mean_carrier <- mean(carrier_change$amt_delay)
-
-# calculating score
-carrier_change <- carrier_change %>% mutate(carrier_score = round(amt_delay - mean_delay,4)) %>%
-  select(UniqueCarrier, carrier_score)
-
-# joining back to train
-train <- inner_join(train, carrier_change, on = 'UniqueCarrier')
-#######################
-# Tail number
-tail_change <- train %>% group_by(TailNum) %>%
-  summarise(n_flights = n(),
-            no_delayed = sum(delay_marker),
-            amt_delay = no_delayed / n_flights)
-
-# mean amount delayed
-mean_tail <- mean(tail_change$amt_delay)
-
-# calculating score
-tail_change <- tail_change %>% mutate(tail_score = round(amt_delay - mean_delay,4)) %>%
-  select(TailNum, tail_score)
-
-# joining back to train
-train <- inner_join(train, tail_change, on = 'TailNum')
-########################
-# Origin
-origin_change <- train %>% group_by(Origin) %>%
-  summarise(n_flights = n(),
-            no_delayed = sum(delay_marker),
-            amt_delay = no_delayed / n_flights)
-
-# mean amount delayed
-mean_origin <- mean(origin_change$amt_delay)
-
-# calculating score
-origin_change <- origin_change %>% mutate(origin_score = round(amt_delay - mean_delay,4)) %>%
-  select(Origin, origin_score)
-
-# joining back to train
-train <- inner_join(train, origin_change, on = 'TailNum')
-########################
-# Destination
-dest_change <- train %>% group_by(Dest) %>%
-  summarise(n_flights = n(),
-            no_delayed = sum(delay_marker),
-            amt_delay = no_delayed / n_flights)
-
-# mean amount delayed
-mean_dest <- mean(dest_change$amt_delay)
-
-# calculating score
-dest_change <- dest_change %>% mutate(dest_score = round(amt_delay - mean_delay,4)) %>%
-  select(Dest, dest_score)
-
-# joining back to train
-train <- inner_join(train, dest_change, on = 'Dest')
-#########################
-
-
-# Cluster analysis --------------------------------------------------------
-# creating cluster dataset
-cluster_df <- train %>% select(index, CRSDepTime:CRSArrTime, CRSElapsedTime, Distance, Month_score:dest_score)
-
-# # dummy glm
-# a <- glm(delay_marker ~ Month_score + Day_score + carrier_score + tail_score + origin_score + dest_score, family = 'binomial', data = train)
-# summary(a)
-
-cluster_df_scale <- scale(cluster_df[2:length(cluster_df)])
-
+#  glm
+log_model <- glm(logistic_form, family = 'binomial', data = train)
+summary(log_model)
